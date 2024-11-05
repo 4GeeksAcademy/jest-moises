a066bc721fc91793c1f328f04efcf2c7cbfe9ca3
moises2872024 <moises.montt287@gmail.com>
2024-11-05 15:14:40 +0000

    jest finalizado

---
+++ app.js
@@ -0,0 +1,20 @@
+// We declare the function with the exact name "fromEuroToDollar"
+const fromEuroToDollar = function(valueInEuro) {
+    // Convert the given valueInEuro to dollars
+    let valueInDollar = valueInEuro * 1.07;
+    // return the dollar value
+    return valueInDollar;
+}
+
+const fromDollarToYen = function(valueInDollar) {
+    let valueInYen = valueInDollar * 151.87;
+    return valueInYen;
+}
+
+const fromYenToPound = function(valueInYen) {
+    let valueInPound = valueInYen * 0.0072;
+    return valueInPound;
+}
+
+// We include fromEuroToDollar here as well because it needs to be exported
+module.exports = { fromEuroToDollar, fromDollarToYen, fromYenToPound  }
